b6394b7e20e07aa5aaf65cd69f2b9860cbff98bd
Viki Zieleniewska <viktoriya.zieleniewska@gmail.com>
2023-05-14 22:01:57 +0200

    Added dropdown section

--- js/app.js
+++ js/app.js
@@ -1,71 +1,3 @@
-// const elSummaryTotal = document.querySelector(".summary__total");
-// // elSummaryTotal.classList.add("open");
-
-// const elListItem = document.querySelectorAll(".list__item");
-// // elListItem.forEach((item) => {
-// //   item.classList.add("open");
-// // });
-
-// const elSelectDropdown = document.querySelector(".select__dropdown");
-// // elSelectDropdown.classList.add("open");
-
-// const selectInput = document.querySelector(".select__input");
-
-// selectInput.addEventListener("click", () => {
-//   elSelectDropdown.classList.add("open");
-// });
-
-// const numberInputs = document.querySelectorAll('input[type="number"]');
-
-// numberInputs.forEach((input) => {
-//   input.addEventListener("change", function () {
-//     const summary = this.id;
-
-//     if (this.value == Math.floor(this.value) && this.value > 0) {
-//       this.classList.remove("invalid");
-//       this.classList.add("open");
-//     } else {
-//       this.classList.add("invalid");
-//       input.value = "";
-//       this.classList.remove("open");
-//     }
-//   });
-// });
-
-// function Calculator(form, summary) {
-//   this.form = form;
-//   this.summary = summary;
-//   this.inputs = form.querySelectorAll("input[type='number']");
-//   this.packageDropdown = form.querySelector(".select__dropdown");
-// }
-
-// Calculator.prototype.init = function () {
-//   const self = this;
-
-//   this.inputs.forEach(function (input) {
-//     input.addEventListener("change", function () {
-//       self.updateSummary();
-//     });
-//   });
-
-//   this.packageDropdown.addEventListener("change", function () {
-//     self.updateSummary();
-//   });
-
-//   this.updateSummary();
-// };
-
-// Calculator.prototype.updateSummary = function () {
-//   let total = 0;
-//   this.inputs.forEach(function (input) {
-//     const maybeNumber = parseInt(input.value);
-//     const test = isNaN(maybeNumber) ? 0 : maybeNumber;
-//     total = total + test;
-//   });
-//   // debugger;
-//   this.summary.innerHTML = "Total: " + total;
-// };
-
 // Handle number inputs
 function validateNumberInput(inputElement, summaryElement) {
   if (
@@ -92,8 +24,27 @@ function updateNumberSummary(inputElement, summaryElement) {
 }
 
 // Handle dropdown
-// TODO validateDropdown()
-// TODO updateDropdownSummary()
+function validateDropdown(inputElement, summaryElement) {
+  const hasValue = !!inputElement.dataset.value;
+  const hasPrice = !!inputElement.dataset.price;
+
+  if (hasValue && hasPrice) {
+    inputElement.classList.remove("invalid");
+    updateDropdownSummary(inputElement, summaryElement);
+    summaryElement.classList.add("open");
+  } else {
+    inputElement.classList.add("invalid");
+    summaryElement.classList.remove("open");
+  }
+}
+
+function updateDropdownSummary(inputElement, summaryElement) {
+  const selectedPackage = inputElement.dataset.value;
+  const price = inputElement.dataset.price;
+
+  summaryElement.children[1].innerHTML = selectedPackage;
+  summaryElement.children[2].innerHTML = `$ ${price}`;
+}
 
 // Handle checkboxes
 function validateCheckbox(inputElement, summaryElement) {
@@ -133,6 +84,9 @@ const ordersInput = document.getElementById("orders");
 const ordersSummary = document.querySelector(`li[data-id="orders"]`);
 
 const packageDropdown = document.getElementById("package");
+const packageSummary = document.querySelector(`li[data-id="package"]`);
+const packageInput = document.querySelector(".select__input");
+const packageOptions = document.querySelector(".select__dropdown");
 
 const accountingCheckbox = document.getElementById("accounting");
 const accountingSummary = document.querySelector(`li[data-id="accounting"]`);
@@ -167,29 +121,22 @@ terminalCheckbox.addEventListener("change", () => {
   validateCheckbox(terminalCheckbox, terminalSummary);
 });
 
-// packageDropdown.addEventListener("change", updateSummary);
-// accountingCheckbox.addEventListener("checked", updateSummary);
-// terminalCheckbox.addEventListener("checked", updateSummary);
+packageInput.addEventListener("click", () => {
+  packageDropdown.classList.add("open");
+});
 
-// updateSummary();
+packageOptions.addEventListener("click", function (event) {
+  const selectedOption = event.target;
+  const selectedValue = selectedOption.dataset.value;
+  const selectedPrice = selectedOption.dataset.price;
 
-// const elSummaryTotal = document.querySelector(".summary__total");
-// const elListItem = document.querySelectorAll(".list__item");
-// const elSelectDropdown = document.querySelector(".select__dropdown");
+  if (selectedValue) {
+    packageInput.innerHTML = selectedOption.innerHTML;
+    packageDropdown.dataset.value = selectedValue;
+    packageDropdown.dataset.price = selectedPrice;
+    packageDropdown.classList.remove("open");
 
-// const form = document.querySelector(".calc__form");
-// const divCalcForm = document.querySelectorAll(".calc__input");
-// const productInput = document.getElementById("products");
-// const orderInput = document.getElementById("orders");
-// const packageDropdown = document.getElementById("package");
-// const selectInput = document.querySelector("select__input");
-// const selectDropdown = document.querySelector("select__dropdown");
-
-// const lableFormCheckBox = document.querySelectorAll(".checkbox");
-// const accountingCheckbox = document.getElementById("accounting");
-// const terminalCheckbox = document.getElementById("terminal");
-
-// const divCalcSummary = document.querySelector(".calc__summary");
-
-// const summary = document.querySelector(".summary__total");
-// const totalPriceElement = document.querySelector("#total-price");
+    validateDropdown(packageDropdown, packageSummary);
+    form.dispatchEvent(new Event("change"));
+  }
+});
